5eb2821eeeb86db80edac31f357373d1b78f0a5e
MiyataHikaru <hikahika2432@gmail.com>
2016-08-17 10:20:11 +0900

    友達が重複しないように

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,9 +2,13 @@ class FriendsController < ApplicationController
 
   def create
     @user = User.find(params[:user_id])
-    follow = current_user.active_relationships.build(followed_id: @user.id)
-    follow.save
-    redirect_to root_path
+    if current_user.active_relationships.find_by(followed_id: @user.id)
+      redirect_to root_path
+    else
+      follow = current_user.active_relationships.build(followed_id: @user.id)
+      follow.save
+      redirect_to root_path
+    end
   end
 
   def destroy
